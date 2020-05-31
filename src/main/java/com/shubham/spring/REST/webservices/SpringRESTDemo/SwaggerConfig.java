411b1ef37a7b25f3a8b667ccc0617f676588abc8
snatu1996 <shubham.natu@cognizant.com>
2020-06-01 00:10:40 +0530

    Enhancing Swagger Documentation

--- src/main/java/com/shubham/spring/REST/webservices/SpringRESTDemo/SwaggerConfig.java
+++ src/main/java/com/shubham/spring/REST/webservices/SpringRESTDemo/SwaggerConfig.java
@@ -2,16 +2,30 @@ package com.shubham.spring.REST.webservices.SpringRESTDemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration //Since this is a configuration file
 @EnableSwagger2  //To enable swagger
 public class SwaggerConfig {
 
+    public static final Contact DEFAULT_CONTACT = new Contact("Shubham Natu","https://github.com/natuShubham96/SpringRestDemo","natushubham96@gmail.com"); //This is the contact info, here. first arg is name, second is url, third is e-mail id
+
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title","Awesome API Documentation","1.0","urn:tos",DEFAULT_CONTACT,"Apache 2.0","http://www.apache.org/licenses/LICENSE-2.0");
+//This is the api info, here, first arg is title, second is description, third is version, fourth is terms of services url, fifth is contact info created above, sixth is license, seventh is license url
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json","application/xml"));
+//Configuring produces and consumes values
+
     @Bean
     public Docket api() {   //Bean to specify documentation type
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO).produces(DEFAULT_PRODUCES_AND_CONSUMES).consumes(DEFAULT_PRODUCES_AND_CONSUMES); //.apiInfo() for changing the api info
     }
 }
--- src/main/java/com/shubham/spring/REST/webservices/SpringRESTDemo/user/User.java
+++ src/main/java/com/shubham/spring/REST/webservices/SpringRESTDemo/user/User.java
@@ -1,17 +1,23 @@
 package com.shubham.spring.REST.webservices.SpringRESTDemo.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All the details about the user are here!!!")  //Provides the description of the resource in the doc
 public class User {
 
     private Integer id;
 
     @Size(min = 3,message = "Name Should Have Atleast 2 Characters")
+    @ApiModelProperty(notes="Name should atleast be 2 characters long!!!")  //For displaying the validation in the doc
     private String name;
 
     @Past
+    @ApiModelProperty(notes="DOB should be in the past!!!")
     private Date DOB;
 
     public User(Integer id, String name, Date DOB) {
